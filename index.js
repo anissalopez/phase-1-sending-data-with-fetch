98043d3a0db00010af54c152ecc207f21c0eea2f
anissalopez <anissa965@gmail.com>
2023-05-13 10:34:06 -0400

    json

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Add your code here
+// function that takes two strings as arguments. One that is userName the other UserEmail
+document.addEventListener('DOMContentLoaded', submitData)
+
+
+function submitData (userName, userEmail){
+    //write a valid Post request that includes URL, headers for Content-type & Accept and body with name and email passed in as arguments to submitData these should be assigned name and email keys
+    let userInfo = {
+        name: userName,
+        email: userEmail
+    }
+
+    let inputObject = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userInfo)
+    }
+
+   return fetch('http://localhost:3000/users', inputObject)
+    .then(function (response){
+        return response.json()
+    })
+    .then(function(object){
+        let p = document.createElement('p')
+         p.textContent = object.id
+         document.body.append(p)
+
+    })
+    .catch(function (error){
+        let p1 = document.createElement('p')
+        p1.textContent = error.message
+        document.body.append(p1)
+    })
+
+}
+
+
+
+
+
